586b09b571d61ae41654c5c7ee223ab7e8d49656
Andranik <andreazure1989@hotmail.fr>
2023-08-23 14:10:20 +0200

    done

--- main.js
+++ main.js
@@ -22,12 +22,23 @@ const render = Render.create({
   },
 });
 
+let restLength = 100;
 
-const cart = Bodies.rectangle(400, innerHeight/2, 100, 20, { isStatic: true, render: { fillStyle: '#f55a3c' } });
+const cart = Bodies.rectangle(200, 240, 40, 20, {
+  friction: 0,
+  restitution: 0,
+  angle: 0,
+  isStatic: false,
+  render: { fillStyle: '#f55a3c' }
+});
 
+cart.w = 40;
+cart.h = 20;
 
-const pole = Bodies.rectangle(400, 520, 50, 50, {
-  restitution: 0.5, density: 0.001,
+const pole = Bodies.rectangle(cart.position.x, cart.position.y - restLength, 20, 20, {
+  friction: 0,
+  restitution: 0.5,
+  isStatic: false,
   collisionFilter: {
     category: 0x0002
   }
@@ -37,14 +48,43 @@ const pole = Bodies.rectangle(400, 520, 50, 50, {
 const poleConstraint = Constraint.create({
   bodyA: cart,
   bodyB: pole,
-  pointB: { x: 0, y: 0 },
-  stiffness: 0,
-  length: 320,
+  length: restLength,
+  stiffness: 1,
+});
+
+let ground = Bodies.rectangle(0, innerHeight / 2, innerWidth * 2, 10, {
+  friction: 0,
+  restitution: 0.5,
+  angle: 0,
+  isStatic: true,
+  collisionFilter: {
+    mask: 0x0001
+  }
 });
 
 
+let leftWall = Bodies.rectangle(5, 0, 100, innerHeight, {
+  friction: 0,
+  restitution: 1,
+  angle: 0,
+  isStatic: true,
+  collisionFilter: {
+    mask: 0x0001
+  }
+});
+
+let rightWall = Bodies.rectangle(innerWidth, 0, 100, innerHeight, {
+  friction: 0,
+  restitution: 1,
+  angle: 0,
+  isStatic: true,
+  collisionFilter: {
+    mask: 0x0001
+  }
+});
+
 // Add all bodies and constraints to the world
-World.add(engine.world, [cart, pole]);
+World.add(engine.world, [cart, pole, ground, rightWall, leftWall]);
 World.add(engine.world, poleConstraint);
 
 // Start the engine
@@ -57,16 +97,17 @@ Render.run(render);
 // Flag to track key presses
 let isLeftKeyDown = false;
 let isRightKeyDown = false;
-const cartSpeed = 400; // Adjust the cart's speed as needed
 
 document.addEventListener("keydown", (event) => {
   const key = event.key;
-
   if (key === "ArrowLeft") {
     isLeftKeyDown = true;
   } else if (key === "ArrowRight") {
     isRightKeyDown = true;
-  }
+  } 
+  if (key === "l" || key === "L") {
+    ai()
+  } 
 });
 
 document.addEventListener("keyup", (event) => {
@@ -79,11 +120,19 @@ document.addEventListener("keyup", (event) => {
   }
 });
 
-// Time variables for delta time calculation
+const randomValue = (minValue, maxValue) => { return Math.random() * (maxValue - minValue) + minValue }
+
+let prevAngle = 0
+let pGain = undefined
+let dGain = undefined
+let solve = false
 let lastTime = 0;
 let accumulatedTime = 0;
 const fixedTimeStep = 1000 / 60; // Target frame rate of 60 FPS
-
+const canvas = document.getElementsByTagName("canvas")[0] // Replace 'canvas' with your canvas element's ID
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = 'rgb(255, 255, 255)'; // Set stroke color to white
+ctx.lineWidth = 4; // Set stroke weight
 
 // Update cart position based on key presses and delta time
 Matter.Events.on(engine, "beforeUpdate", (event) => {
@@ -94,13 +143,53 @@ Matter.Events.on(engine, "beforeUpdate", (event) => {
   accumulatedTime += deltaTime;
 
   while (accumulatedTime >= fixedTimeStep) {
-    if (isLeftKeyDown && cart.position.x > 50) {
-      Matter.Body.translate(cart, { x: -cartSpeed * (fixedTimeStep / 1000), y: 0 });
+
+
+    let arm = { x: 0, y: 0 }
+    arm.x = pole.position.x - cart.position.x;
+    arm.y = pole.position.y - cart.position.y;
+    let angle = Math.atan2(arm.y, arm.x) + Math.PI / 2;
+    let dt = 1;
+    let angleV = (angle - prevAngle) / dt;
+    prevAngle = angle;
+  
+    let error = 0 - angle;
+
+    if(solve){
+      pGain = randomValue(0.005, 0.001)
+      dGain = randomValue(0.0025, 0.001)
+      let fx = -1 * pGain * error - dGain * -angleV;
+      console.log(fx);
+      const force = {x: fx, y: 0};
+      Matter.Body.applyForce(cart, cart.position, force);
     }
-    if (isRightKeyDown && cart.position.x < innerWidth - 50) {
-      Matter.Body.translate(cart, { x: cartSpeed * (fixedTimeStep / 1000), y: 0 });
+
+   
+    if (isLeftKeyDown) {
+      const force = { x: -0.002, y: 0 }
+      Matter.Body.applyForce(cart, cart.position, force);
     }
+    if (isRightKeyDown) {
+      const force = { x: 0.002, y: 0 }
+      Matter.Body.applyForce(cart, cart.position, force);
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(cart.position.x, cart.position.y);
+    ctx.lineTo(pole.position.x, pole.position.y);
+    ctx.stroke();
 
     accumulatedTime -= fixedTimeStep;
   }
 });
+
+
+
+
+function ai() {
+  if(!solve){
+    solve = true
+  }else{
+    solve = false
+  }
+}
